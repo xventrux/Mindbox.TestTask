f5fc9db6ea5cf72ab43f9be826ec9771eb39cb2a
Vyacheslav <vvyacheslavx@gmail.com>
2022-07-01 19:15:42 +0300

    В классе треугольника реализовал интерфейс фигуры

---
+++ FiguresLibrary/Figures/Triangle.cs
@@ -0,0 +1,32 @@
+﻿using FiguresLibrary.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiguresLibrary.Figures
+{
+    public class Triangle : IFigure
+    {
+        #region стороны треугольника
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+        #endregion
+
+        public Triangle(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public double getArea()
+        {
+            double p = (A + B + C) / 2;
+
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
