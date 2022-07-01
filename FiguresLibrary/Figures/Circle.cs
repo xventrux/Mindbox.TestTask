acd65bf302ec3e48fc18ae1c7d79d2395356f24b
Vyacheslav <vvyacheslavx@gmail.com>
2022-07-01 19:06:45 +0300

    В классе Circle реализовал интерфейс IFigure

---
+++ FiguresLibrary/Figures/Circle.cs
@@ -0,0 +1,24 @@
+﻿using FiguresLibrary.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiguresLibrary.Figures
+{
+    public class Circle : IFigure
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double getArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+}
